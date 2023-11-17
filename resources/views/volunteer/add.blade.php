11f9ffa5037594f9a2627462ea5f3de2906375e8
Imalia Rosyida <imaliarosidah@gmail.com>
2023-11-17 23:14:49 +0700

    perbaikan add dan index

--- resources/views/volunteer/add.blade.php
+++ resources/views/volunteer/add.blade.php
@@ -1,35 +1,183 @@
 @extends('layouts.volunteer')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-<h3>Form Input Volunteer</h3>
-<form method="post" action="{{route('add volunteer')}}">
-  @csrf
-  <div class="form-group">
-    <label>User ID</label>
-    <input type="text" name="user_id" class="form-control" placeholder="Nama Santri" required="">
-  </div>
-  <div class="form-group">
-    <label>NIM</label>
-    <input type="text" name="nim" class="form-control" placeholder="Tempat Lahir" required="">
-  </div>
-  <div class="form-group">
-    <label>Devisi</label>
-    <input type="text" name="devisi" class="form-control" placeholder="Tanggal Lahir" required="">
-  </div>
-  <div class="form-group">
-    <label>No WA</label>
-    <input type="text" name="no_wa" class="form-control" placeholder="Tanggal Lahir" required="">
-  </div>
-  <div class="form-group">
-    <label>Link</label>
-    <input type="text" name="link" class="form-control" placeholder="No. Hp" required="">
-  </div>
-  <div class="form-group">
-    <label>Status Pendaftaran</label>
-    <input type="text" name="status_pendaftaran" class="form-control" placeholder="No. Hp" required="">
-  </div>
-  <div class="form-group text-right">
-    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Simpan Data</button>
-  </div>
-</form>
-@endsection
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Modernize Free</title>
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+  <link rel="stylesheet" href="{{ asset('/css/styles.min.css') }}"/>
+  <link rel="stylesheet" href="{{ asset('/css/custom.css') }}"/>
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <!-- Sidebar Start -->
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./index.html" class="text-nowrap logo-img">
+            <img src="../assets/images/logos/dark-logo.svg" width="180" alt="" />
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+            <li class="nav-small-cap">
+              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span class="hide-menu"></span>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./index.html" aria-expanded="false">
+                <span>
+                  <i class="ti ti-layout-dashboard"></i>
+                </span>
+                <span class="hide-menu">Dashboard</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-keuangan.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-moneybag"></i>
+                  </span>
+                  <span class="hide-menu">Keuangan</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/admin/volunteers" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-users"></i>
+                  </span>
+                  <span class="hide-menu">Volunteer</span>
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-donasi.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-wallet"></i>
+                  </span>
+                  <span class="hide-menu">Donasi</span>
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-galeri.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-photo"></i>
+                  </span>
+                  <span class="hide-menu">Galeri</span>
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-publikasi.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-news"></i>
+                  </span>
+                  <span class="hide-menu">Berita</span>
+                </a>
+              </li>
+            
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!--  Sidebar End -->
+  @section('content')
+  <div class="body-wrapper">
+      <!--  Header Start -->
+      <header class="app-header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <ul class="navbar-nav">
+            <li class="nav-item d-block d-xl-none">
+              <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
+                <i class="ti ti-menu-2"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-icon-hover" href="javascript:void(0)">
+                <i class="ti ti-bell-ringing"></i>
+                <div class="notification bg-primary rounded-circle"></div>
+              </a>
+            </li>
+          </ul>
+          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+              <li class="nav-item dropdown">
+                <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  <img src="{{ asset('/images/profile/user-1.jpg') }}" alt="" width="35" height="35" class="rounded-circle">
+                </a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
+                  <div class="message-body">
+                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-user fs-6"></i>
+                      <p class="mb-0 fs-3">My Profile</p>
+                    </a>
+                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-mail fs-6"></i>
+                      <p class="mb-0 fs-3">My Account</p>
+                    </a>
+                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-list-check fs-6"></i>
+                      <p class="mb-0 fs-3">My Task</p>
+                    </a>
+                    <a href="./authentication-login.html" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <!--  Header End -->
+      <div class="container-fluid">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title fw-semibold mb-4">Forms</h5>
+              <div class="card">
+                <div class="card-body">
+                  <form method="post" action="{{route('simpan volunteer')}}">
+                    <div class="mb-3">
+                      <label for="user_id" class="form-label">User ID</label>
+                      <input type="text" class="form-control" id="user_id" name="user_id">
+                    </div>
+                    <div class="mb-3">
+                      <label for="nim" class="form-label">NIM</label>
+                      <input type="text" class="form-control" id="nim" nama="nim">
+                    </div>
+                    <div class="mb-3">
+                      <label for="devisi" class="form-label">Devisi</label>
+                      <input type="text" class="form-control" id="devisi" nama="devisi">
+                    </div>
+                    <div class="mb-3">
+                      <label for="no_wa" class="form-label">No Wa</label>
+                      <input type="text" class="form-control" id="no_wa" nama="no_wa">
+                    </div>
+                    <div class="mb-3">
+                      <label for="link" class="form-label">Link CV</label>
+                      <input type="text" class="form-control" id="link" nama="link">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endsection
+  <script src="{{asset ('/libs/jquery/dist/jquery.min.js') }}"></script>
+  <script src="{{asset ('/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{asset ('/js/sidebarmenu.js') }}"></script>
+  <script src="{{asset ('/js/app.min.js') }}"></script>
+  <script src="{{asset ('/libs/simplebar/dist/simplebar.js') }}"></script>
+</body>
+</html>
--- resources/views/volunteer/index.blade.php
+++ resources/views/volunteer/index.blade.php
@@ -1,29 +1,209 @@
 @extends('layouts.volunteer')
-@section('content')
-    <table>
-		<tr>
-			<th>user ID</th>
-			<th>nim</th>
-			<th>devisi</th>
-			<th>No Wa</th>
-			<th>Link</th>
-            <th>Status Pendaftaran</th>
-            <th>Created at</th>
-            <th>Update at</th>
-		</tr>
-    @foreach($volunteers as $v)
-        <tr>
-            <td>{{ $v->user_id }}</td>
-            <td>{{ $v->nim}}</td>
-            <td>{{ $v->devisi }}</td>    
-            <td>{{ $v->no_wa }}</td>
-            <td>{{ $v->link }}</td>
-            <td>{{ $v->status_pendaftaran }}</td>
-            <td>{{ $v->created_at }}</td>
-            <td>{{ $v->updated_at }}</td>
-        </tr>
-    @endforeach
-    </table>
-    <h3>Tampil Data Satri</h3>
-    <a class="btn btn-success" href="{{route('add volunteer')}}"><i class="fa fa-plus"></i> Tambah Santri</a>
-@endsection
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Modernize Free</title>
+  <link rel="shortcut icon" type="image/png" href="{{ asset('/images/logos/favicon.png') }}" />
+  <link rel="stylesheet" href="{{ asset('/css/styles.min.css') }}"/>
+  <link rel="stylesheet" href="{{ asset('/css/custom.css') }}"/>
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <!-- Sidebar Start -->
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./index.html" class="text-nowrap logo-img">
+            <img src="../assets/images/logos/dark-logo.svg" width="180" alt="" />
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+            <li class="nav-small-cap">
+              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span class="hide-menu"></span>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./index.html" aria-expanded="false">
+                <span>
+                  <i class="ti ti-layout-dashboard"></i>
+                </span>
+                <span class="hide-menu">Dashboard</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-keuangan.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-moneybag"></i>
+                  </span>
+                  <span class="hide-menu">Keuangan</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-volunteer.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-users"></i>
+                  </span>
+                  <span class="hide-menu">Volunteer</span>
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-donasi.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-wallet"></i>
+                  </span>
+                  <span class="hide-menu">Donasi</span>
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-galeri.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-photo"></i>
+                  </span>
+                  <span class="hide-menu">Galeri</span>
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="./ui-publikasi.html" aria-expanded="false">
+                  <span>
+                    <i class="ti ti-news"></i>
+                  </span>
+                  <span class="hide-menu">Berita</span>
+                </a>
+              </li>
+            
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!--  Sidebar End -->
+    @section('content')
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+      <!--  Header Start -->
+      
+      <!--  Header End -->
+      <div class="container-fluid">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center mb-4">
+                    <h5 class="card-title fw-semibold">Tabel Daftar Volunteer</h5>
+                    <div class="d-flex align-items-center">
+                        <div class="dropdown me-3">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="sortingDropdown"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Urut dari yang terbaru
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="sortingDropdown">
+                                <li><a class="dropdown-item" href="#">Urut dari yang terbaru</a></li>
+                                <li><a class="dropdown-item" href="#">Urut dari yang terlama</a></li>
+                            </ul>
+                        </div>
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Cari tahun pendaftaran" aria-label="Search" aria-describedby="searchButton">
+                            <button class="btn btn-primary" type="button" id="searchButton">Cari</button>
+                        </div>
+                    </div>
+                </div>
+              <div class="card">
+                <div class="table-responsive">
+                    <table class="table text-nowrap mb-0 align-middle">
+                      <thead class="text-dark fs-4">
+                        <tr>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">User ID</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">NIM</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">Divisi</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">No WA</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">Link</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">Status Pendaftara</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">Created At</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">Update At</h6>
+                          </th>
+                          <th class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0">Action</h6>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      @foreach($volunteers as $v)
+                        <tr>
+                            <td class="border-bottom-0">
+                                <h6 class="fw-semibold mb-0">{{ $v->user_id }}</h6>
+                            </td>
+                            <td class="border-bottom-0">
+                                <h6 class="fw-semibold mb-1">{{ $v->nim}}</h6>                         
+                            </td>
+                            <td class="border-bottom-0">
+                                <h6 class="fw-semibold mb-1">{{ $v->devisi}}</h6>                         
+                            </td>
+                            <td class="border-bottom-0">
+                                <p class="mb-0 fw-normal">{{ $v->no_wa }}</p>
+                            </td>
+                            <td class="border-bottom-0">
+                                <h6 class="fw-semibold mb-0 fs-4"><a href="https://www.stis.ac.id/">link cv</a></a></h6>
+                            </td>
+                            <td class="border-bottom-0">
+                                <h6 class="fw-semibold mb-0">{{ $v->status_pendaftaran }}</h6>
+                            </td>
+                            <td class="border-bottom-0">
+                                <h6 class="fw-semibold mb-1">{{ $v->created_at }}</h6>                         
+                            </td>
+                            <td class="border-bottom-0">
+                                <p class="mb-0 fw-normal">{{ $v->updated_at }}</p>
+                            </td>
+                            <td class="border-bottom-0">
+                              <button type="button" class="btn btn-primary mr-2" data-toggle="modal" data-target="#editModal">
+                                Edit
+                              </button>
+                              <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#hapusModal">
+                                Hapus
+                              </button>
+                            </td>
+                        </tr>    
+                      @endforeach                        
+                      </tbody>
+                    </table>
+                </div>
+            </div>
+            <a class="btn btn-success" href="{{route('add volunteer')}}"><i class="fa fa-plus"></i> Tambah Volunteer</a>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  @endsection
+  <script src="{{asset ('/libs/jquery/dist/jquery.min.js') }}"></script>
+  <script src="{{asset ('/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{asset ('/js/sidebarmenu.js') }}"></script>
+  <script src="{{asset ('/js/app.min.js') }}"></script>
+  <script src="{{asset ('/libs/simplebar/dist/simplebar.js') }}"></script>
+</body>
+</html>
